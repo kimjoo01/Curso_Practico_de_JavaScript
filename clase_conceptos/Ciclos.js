fe17bbf26e983d42d8d9fba2c99871eb02459461
kimjoo <kimjoo278@gmail.com>
2023-08-22 17:40:58 -0500

    Ciclos

---
+++ clase_conceptos/Ciclos.js
@@ -0,0 +1,20 @@
+let i = 0;
+let n = 10;
+
+while(i < 5){
+    console.log("El valor de i es: " + i);
+    i++;
+}
+
+while(n >= 2){
+    console.log("El valor de n es: " + n);
+    n--;
+}
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("El valor de i es: " + i);
+// }
+
+// for (let i = 10; i >= 2; i--) {
+//     console.log("El valor de i es: " + i);
+// }
