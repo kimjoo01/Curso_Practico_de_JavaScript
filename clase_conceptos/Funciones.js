7167488656400ffc6bbd16aad4111cb8e61ba6e6
kimjoo <kimjoo278@gmail.com>
2023-08-22 16:37:35 -0500

    Funciones

---
+++ clase_conceptos/Funciones.js
@@ -0,0 +1,10 @@
+const nickname = "juandc";
+
+function completeName(name, lastname){
+    name = "Juan David";
+    lastname = "Castro Gallego";
+    complete = name + lastname;
+    return complete;
+};
+
+console.log("Mi nombre es " + completeName(complete) + ", pero prefiero que me digas " + nickname + ".");
