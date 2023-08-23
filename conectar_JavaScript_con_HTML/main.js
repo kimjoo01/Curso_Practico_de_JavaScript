bd229f1ad74651b9f101bb07ec0234e23e361ca7
kimjoo <kimjoo278@gmail.com>
2023-08-23 14:55:28 -0500

    Escribiendo HTML desde JavaScript

--- conectar_JavaScript_con_HTML/main.js
+++ conectar_JavaScript_con_HTML/main.js
@@ -3,6 +3,7 @@ const p = document.querySelector('p');
 const parrafito = document.querySelector('.parrafito');
 const pid = document.querySelector('#pid');
 const input = document.querySelector('input');
+const img = document.createElement('img');
 
 console.log(h1);
 
@@ -13,3 +14,16 @@ console.log({
     pid,
     input
 });
+
+h1.innerHTML = "La oruga pepe";
+// console.log (h1.getAttribute('class'));
+// h1.setAttribute('class' , 'verde');
+
+h1.classList.add('rojo');
+h1.classList.remove('nexodo');
+
+input.value = "512365";
+
+img.src = "https://img.redbull.com/images/c_fill,w_2880,h_1460,g_auto,f_auto,q_auto/redbullcom/2023/4/19/mxdrfhmrauci8vbengho/rb19-main-hero";
+
+pid.append(img);
