9c1a6bb389eb542466f66bbf42326a0394b76a2c
kimjoo <kimjoo278@gmail.com>
2023-08-24 13:19:18 -0500

    addEventListener

--- conectar_JavaScript_con_HTML/main.js
+++ conectar_JavaScript_con_HTML/main.js
@@ -3,10 +3,23 @@ const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#suma');
 const parrafo = document.querySelector('#parrafo');
-const operacion = document.querySelector('#operacion')
+const operacion = document.querySelector('#operacion');
+const suma_but = document.querySelector('#suma');
+const resta_but = document.querySelector('#resta');
+const multi_but = document.querySelector('#multi');
+const divi_but = document.querySelector('#divi');
+const form = document.querySelector('#form');
+
+suma_but.addEventListener('click', suma);
+resta_but.addEventListener('click', resta);
+multi_but.addEventListener('click', multi);
+divi_but.addEventListener('click', divi);
+
+// form.addEventListener('submit', suma)
 
 function suma () {
     const suma = Number(input1.value) + Number(input2.value);
+    // event.preventDefault();
     parrafo.innerText = "= " + suma;
     operacion.innerText = "+";
 }
