9b7689d4018c966bf92aa4d05a3d7cd896d795dd
kimjoo <kimjoo278@gmail.com>
2023-08-24 16:36:52 -0500

    Fusión del menú en mobile

--- fusion_e_incorporacion_de_JS/main.js
+++ fusion_e_incorporacion_de_JS/main.js
@@ -1,9 +1,16 @@
 const navUser = document.querySelector('.navbar_user');
 const desktop_menu = document.querySelector('.desktop_menu');
+const menu_ham_icon = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile_menu');
 
 navUser.addEventListener('click', toggle_Desktop_Menu);
+menu_ham_icon.addEventListener('click', toggle_Mobile_Menu);
 
 function toggle_Desktop_Menu (){
     desktop_menu.classList.toggle('inactive');
     navUser.classList.toggle('bold');
+}
+
+function toggle_Mobile_Menu (){
+    mobile_menu.classList.toggle('inactive');
 }
